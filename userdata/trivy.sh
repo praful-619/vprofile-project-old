ff0962cb6cfa131cc161fdbce333c5d090e614b9
Steps-Tech <stepstechnology903@gmail.com>
2024-06-15 18:41:48 +0530

    Update trivy.sh

--- userdata/trivy.sh
+++ userdata/trivy.sh
@@ -12,14 +12,14 @@ sudo apt-get install trivy
 
 ---COMMANDS---
 
-trivy image imagename
+trivy image imagename    # To scan the Image
 
-trivy fs --security-checks vuln,config   Folder_name_OR_Path
+trivy fs --security-checks vuln,config   Folder_name_OR_Path  #Scan the Local Git Repository
 
-trivy image --severity HIGH,CRITICAL image_name
+trivy image --severity HIGH,CRITICAL image_name  #This will show only HIGH/CRITICAL Vulnerability
 
-trivy image -f json -o results.json image_name
+trivy image -f json -o results.json image_name  # It will generate a Report in json format and store in result.json -f:file -o:output
 
-trivy repo repo-url
+trivy repo repo-url #Scan the Remote Git Repository
 
-trivy k8s --report summary cluster
+trivy k8s --report summary cluster #Scan the K8S Cluster
