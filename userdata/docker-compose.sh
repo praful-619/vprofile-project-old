d3051b47b66d8afd32f7503833a3fd867f8e136a
Steps-Tech <stepstechnology903@gmail.com>
2024-06-16 08:06:37 +0530

    Create docker-compose.sh

---
+++ userdata/docker-compose.sh
@@ -0,0 +1,11 @@
+#Installation Commands
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
+#Command to run the Compose file
+touch docker-compose.yml
+docker compose up  
+    or
+docker compose up -d
